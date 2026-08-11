f44cf4327f2a32b6adc5b0be365ea5f4e27ba1fc
nutt <nd7v2gmp8t@privaterelay.appleid.com>
2026-08-12 03:03:16 +0700

    test1

--- src/main.js
+++ src/main.js
@@ -12,7 +12,21 @@ async function renderApp(wallet) {
         const app = document.querySelector('#app');
         app.innerHTML = /*html*/`
             ${navbar}
-            Change Something
+            <button id="installBtn" class="bg-blue-300 p-3 rounded text-white">Install App</button>
         `;
+        settingPWA();
     }
+}
+
+async function settingPWA() {
+    const installBtn = document.getElementById('installBtn');
+
+    installBtn.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        console.log(`User response: ${outcome}`);
+        deferredPrompt = null;
+    }
+    });
 }
